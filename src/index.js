98cb845ceaa08c37c60b8a693ff10654cf9569de
mangesh vispute <mangesh2003vispute@gmail.com>
2024-01-02 08:31:11 +0530

    utils code added

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 dotenv.config({
   path: "./env",
 });
@@ -22,4 +23,17 @@ dotenv.config({
 // })();
 
 // connecting to the database method2
-connectDB();
+connectDB()
+  .then(() => {
+    app.on("error", (error) => {
+      console.log("MongoDB connection error", error);
+      throw error;
+    });
+    console.log("MongoDB connected");
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server listening on port ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("error:", error);
+  });
